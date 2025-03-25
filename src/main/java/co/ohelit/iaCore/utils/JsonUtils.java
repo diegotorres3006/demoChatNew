df3411a33cef91ea55609dbd8f6de8c27ad52b9c
diegotorres3006 <datorres@unimonserrate.edu.co>
2025-03-25 18:36:38 -0500

    Feature: Util  getNestedValue para if anidados

--- src/main/java/co/ohelit/iaCore/utils/JsonUtils.java
+++ src/main/java/co/ohelit/iaCore/utils/JsonUtils.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 @Component
 public class JsonUtils {
@@ -15,5 +19,19 @@ public class JsonUtils {
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Error al convertir el objeto a JSON", e);
         }
+    }
+
+    public static <T> Optional<T> getNestedValue(Map<String, Object> map, String... keys) {
+        Object value = map;
+        for (String key : keys) {
+            if (value instanceof Map) {
+                value = ((Map<?, ?>) value).get(key);
+            } else if (value instanceof List<?> list && !list.isEmpty() && list.get(0) instanceof Map) {
+                value = list.get(0);
+            } else {
+                return Optional.empty();
+            }
+        }
+        return Optional.ofNullable((T) value);
     }
 }
